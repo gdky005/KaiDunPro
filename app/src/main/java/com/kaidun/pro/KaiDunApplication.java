fb85ec0c7d38c4383f033a7b21534de9563f198c
qingw <qingw@jumei.com>
2018-03-06 16:31:25 +0800

    1. 删除无用的代码； 2. 删除 bugtags。

--- app/src/main/java/com/kaidun/pro/KaiDunApplication.java
+++ app/src/main/java/com/kaidun/pro/KaiDunApplication.java
@@ -6,14 +6,7 @@ import android.os.Build;
 import android.os.StrictMode;
 import android.support.multidex.MultiDex;
 
-import com.bugtags.library.Bugtags;
 import com.facebook.drawee.backends.pipeline.Fresco;
-import com.facebook.stetho.DumperPluginsProvider;
-import com.facebook.stetho.Stetho;
-import com.facebook.stetho.dumpapp.DumperPlugin;
-import com.facebook.stetho.dumpapp.plugins.HprofDumperPlugin;
-import com.facebook.stetho.okhttp3.StethoInterceptor;
-import com.kaidun.pro.managers.KDConnectionManager;
 
 import team.zhuoke.sdk.ZKBase;
 
@@ -31,28 +24,7 @@ public class KaiDunApplication extends Application {
 
         mContext = this;
         Fresco.initialize(mContext);
-
-        //在这里初始化
-        try {
-            Bugtags.start("fedc05f31e8d57fd6a20bb9d1fa7349c", this, Bugtags.BTGInvocationEventBubble);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
         ZKBase.init(this, BuildConfig.DEBUG);
-
-        Stetho.initialize(Stetho.newInitializerBuilder(mContext)
-                .enableDumpapp(new DumperPluginsProvider() {
-                    @Override
-                    public Iterable<DumperPlugin> get() {
-                        return new Stetho.DefaultDumperPluginsBuilder(mContext)
-                                .provide(new HprofDumperPlugin(mContext))
-                                .finish();
-                    }
-                })
-                .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(mContext))
-                .build());
-
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
             // android 7.0系统解决拍照的问题
@@ -60,9 +32,6 @@ public class KaiDunApplication extends Application {
             StrictMode.setVmPolicy(builder.build());
             builder.detectFileUriExposure();
         }
-
-        //添加 Stetho 的拦截器
-        KDConnectionManager.getInstance().getBuilder().addNetworkInterceptor(new StethoInterceptor());
     }
 
     /**
--- app/src/main/java/com/kaidun/pro/activity/KDBaseActivity.java
+++ app/src/main/java/com/kaidun/pro/activity/KDBaseActivity.java
@@ -1,16 +1,5 @@
 package com.kaidun.pro.activity;
 
-import android.graphics.Color;
-import android.os.CountDownTimer;
-import android.text.TextUtils;
-import android.view.MotionEvent;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.bugtags.library.Bugtags;
-import com.kaidun.pro.managers.ZKManager;
-
 import team.zhuoke.sdk.base.BaseActivity;
 
 /**
@@ -18,69 +7,5 @@ import team.zhuoke.sdk.base.BaseActivity;
  * Created by WangQing on 2018/1/30.
  */
 public abstract class KDBaseActivity extends BaseActivity {
-
-    private CountDownTimer timer;
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        //注：回调 1
-        Bugtags.onResume(this);
-        ZKManager.getInstance().refresh();
-        initStatus();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        //注：回调 2
-        Bugtags.onPause(this);
-    }
-
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent event) {
-        //注：回调 3
-        Bugtags.onDispatchTouchEvent(this, event);
-        return super.dispatchTouchEvent(event);
-    }
-
-    private void initStatus() {
-        if (!TextUtils.isEmpty(ZKManager.getInstance().getWatermarkText())) {
-            TextView tv = new TextView(this);
-            tv.setText(ZKManager.getInstance().getWatermarkText());
-            tv.setTextSize(25);
-            tv.setTextColor(Color.BLACK);
-            tv.setPadding(0, getResources().getDisplayMetrics().heightPixels / 2, 0, 0);
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            addContentView(tv, params);
-        }
-
-        if (!TextUtils.isEmpty(ZKManager.getInstance().getWarningText())) {
-            Toast.makeText(mContext, ZKManager.getInstance().getWarningText(), Toast.LENGTH_SHORT).show();
-        }
-
-        if (ZKManager.getInstance().isAppState()) {
-            timer = new CountDownTimer(1000 * 60, 1000) {
-
-                @Override
-                public void onTick(long millisUntilFinished) {
-
-                }
-
-                @Override
-                public void onFinish() {
-                    Toast.makeText(mContext, "应用马上退出，付费完成可以享受完整功能 ！！！", Toast.LENGTH_SHORT).show();
-                    getWindow().getDecorView().postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            System.exit(0);
-                        }
-                    }, 2000);
-                    timer = null;
-                }
-            };
-            timer.start();
-        }
-    }
 
 }
